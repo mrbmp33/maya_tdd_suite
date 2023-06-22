b97b74768faf1245fc1f5ce507ad552b5d5ec9ba
Bernat Mas <bernatmas33@gmail.com>
2023-06-22 21:57:50 +0200

    updating license and gitignore
    Signed-off-by: Bernat Mas <bernatmas33@gmail.com>

---
+++ sandbox.py
@@ -0,0 +1,11 @@
+# from utils import config_loader
+import os
+import dotenv
+import pathlib
+
+if __name__ == '__main__':
+    print(pathlib.Path(__file__))
+    # dotenv.load_dotenv()
+    # _config = config_loader.load_config(os.environ['TDD_CONFIG_FILE'])
+    #
+    # print(_config)
