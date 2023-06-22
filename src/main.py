cdd39b3370060e5aedc999d081c44badfee2e2ae
Bernat Mas <bernatmas33@gmail.com>
2023-06-22 23:08:08 +0200

    changed way to initialize subprocess and developed test expected to fail.
    Signed-off-by: Bernat Mas <bernatmas33@gmail.com>

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ if root_dir not in sys.path:
     sys.path.append(root_dir)
     os.environ.setdefault('MAYA_TDD_ROOT_DIR', root_dir)
 
+
 def main():
     """Entry point to run python tests for a Maya module from the command line.
 
@@ -20,42 +21,35 @@ def main():
         """
     from src.run_tests import run_tests
     from src.utils import maya_locations, config_loader
-
+    
     # Define and get the arguments from the command line
     parser = argparse.ArgumentParser(description='Runs unit tests for a Maya module')
     parser.add_argument('-m', '--maya', help='Maya version', type=int, default=2022)
-
+    
     parsed_args = parser.parse_args()
-
+    
     # Get the test runner module
     run_tests_py = inspect.getfile(run_tests)
-
+    
     # Build the command that will launch the test runner module using mayapy
     cmd = [maya_locations.mayapy(parsed_args.maya), run_tests_py]
-
+    
     if not pathlib.Path(cmd[0]).exists():
         raise RuntimeError(f"Couldn't find a valid Maya executable with the given version: {parsed_args.maya}")
-
+    
     # Create clean prefs and prepare sterile environ for testing
     maya_app_dir = maya_locations.create_clean_maya_app_dir()
-
+    
     os.environ['MAYA_APP_DIR'] = maya_app_dir
     # Clear out any MAYA_SCRIPT_PATH value so that we know we're in a clean env.
     os.environ['MAYA_SCRIPT_PATH'] = ''
-
+    
     # Paths to inspect.
     tests_dirs: list = config_loader.load_config()['paths']['tests']
     os.environ['MAYA_MODULE_PATH'] = f"{os.pathsep}".join(tests_dirs)
-
+    
     try:
-        process = subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True
-        )
-        out, error = process.communicate()
-        print(out)
+        subprocess.check_call(cmd)
     except subprocess.CalledProcessError as pe:
         raise pe
     finally:
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -12,48 +12,53 @@ _logger.setLevel(logging.WARNING)
 
 
 class DummyTestCase(maya_test.MayaTestCase):
-
+    
     def test_get_tmp_file_name(self):
         """Generate a bunch of temporary files to test that the naming convention persists."""
-
+        
         # Get the initial length of files created to compare with the ending one
         len_to_delete_init = len(self.__class__.files_created)
-
+        
         uuid = 'hi'
-
+        
         # Get the file name from the file name builder. This will also create the tmp dir.
         tmp_file_name = pathlib.Path(self.get_temp_filename(f"{uuid}.log"))
-
+        
         max_iterations = 10
-
+        
         num = 0
-
+        
         while num <= max_iterations:
             expected_file_name = pathlib.Path(_config["paths"]["tmp"]) / f"{uuid}.{num}.log"
-
+            
             try:
                 # Expected to break if the file already exists
                 expected_file_name.touch(exist_ok=False)
-
+                
                 self.assertEqual(expected_file_name, tmp_file_name)
                 self.assertGreater(len(self.__class__.files_created), len_to_delete_init)
-
+                
                 return
-
+            
             except OSError:
                 _logger.debug(f"File: {expected_file_name} already exists. Incrementing name...")
                 num += 1
-
+        
         # _logger.warning("There were more leftover files in the tmp dir than the max iterations to test. You should"
         #                 "consider cleaning them up.")
-
+    
     def test_some_other_thing(self):
         self.assertTrue(True)
 
-class AAA(maya_test.MayaTestCase):
 
-    def test_asdai(self):
+class OtherTestCase(maya_test.MayaTestCase):
+    
+    def test_passing(self):
         self.assertEqual(1, 1)
+    
+    @unittest.expectedFailure
+    def test_failure(self):
+        self.assertEqual(0, 1)
 
 
 if __name__ == '__main__':
