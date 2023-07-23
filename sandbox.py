33e4f5b694fa301841b76543f81937cbc51299f5
Bernat Mas <bernatmas33@gmail.com>
2023-07-23 11:58:00 +0200

    adding icons folder to vcs
    Signed-off-by: Bernat Mas <bernatmas33@gmail.com>

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,20 @@
-# from utils import config_loader
-import os
-import dotenv
 import pathlib
+import sys
+
+
+def find_importable_root(path):
+    directory = pathlib.Path(path)
+    module = list(directory.glob("*.py"))[0]
+
+    for each in module.parents:
+        if '__init__.py' not in [x.name for x in each.iterdir() if x.is_file()]:
+            return str(each)
+
 
 if __name__ == '__main__':
-    print(pathlib.Path(__file__))
-    # dotenv.load_dotenv()
-    # _config = config_loader.load_config(os.environ['TDD_CONFIG_FILE'])
-    #
-    # print(_config)
+    p = find_importable_root(r'/home/bernat/Dades/Documents/01_personal_projects/FKSolver/fks/tests')
+    if p not in sys.path:
+        print(p)
+        sys.path.insert(0, p)
+
+    import fks.tests.test_weakDataObjects
